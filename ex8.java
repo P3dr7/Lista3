62c6b8eaf83da11d29f1f5cf193496bb90e69976
P3dr7 <100977505+P3dr7@users.noreply.github.com>
2022-08-15 14:48:11 -0300

    Update ex8.java

--- ex8.java
+++ ex8.java
@@ -2,24 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompanypedro.lista3;
+package Lista3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author labinfo
- */
-public class ex8 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int n1,n2,n3,n4,n5;
-        float media;
-        System.out.println("Informe o 1º Número: ");
-        n1 = sc.nextInt();
-        System.out.println("Informe o 2º Número: ");
-        n2 = sc.nextInt();
-        
-                
-    }
+public class e8  {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+        float[] n = new float[5];
+        float media=0, soma=0;
+        for (int i=0; i< n.length; i++) {
+        	System.out.println("Digite o "+(i+1)+"º número:");
+        	n[i] = ler.nextFloat();
+        	soma = n[i] + soma;
+        }
+        System.out.println("A soma dos números é igual à: "+soma);
+        media = soma/5;
+        System.out.println("A média dos números é igual à: "+media);
+	}
 }
